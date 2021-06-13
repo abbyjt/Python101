d05ecccf3910df81158913f3bfa8c0ea15f32712
abbyjt <boo.townsend@icloud.com>
2021-06-13 14:27:57 +0000

    comment changez

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,4 @@
+#print phrases
 print("I think im doing this right?")
 print("i dont think i very good at this")
 print("Hi abby")
--- Variables.py
+++ Variables.py
@@ -1,10 +1,13 @@
+#how to set and access (access is print btw) variables in python
 var1 = "hello"
 var2 = "world"
 print(var1, var2)
 
+#array, set an array and access 
 arr1 = [var1, var2, 1, True]
 print(arr1)
 print(arr1[0], arr1[1])
 
+#2d array, set and access
 array2D = [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
 print (array2D[0][0])
