c9152617628ed7be6844f894ec99f4e20d9a1a2b
abbyjt <boo.townsend@icloud.com>
2021-06-13 14:19:32 +0000

    changes 1

---
+++ Variables.py
@@ -0,0 +1,10 @@
+var1 = "hello"
+var2 = "world"
+print(var1, var2)
+
+arr1 = [var1, var2, 1, True]
+print(arr1)
+print(arr1[0], arr1[1])
+
+array2D = [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
+print (array2D[0][0])
